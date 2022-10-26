ecc8d4e8bdf516f2dc1217d2c389fbb25c79e834
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-10-26 17:41:03 -0500

    updated al changes to bonus computation file

--- routes/computation.js
+++ routes/computation.js
@@ -4,9 +4,13 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var randomNum = Math.floor(Math.random() * 10);
-    res.render('computation', { title: 'Jyothsna Pamarthi works with computation', result:`Math.expm1()  applied to ${randomNum} is ${Math.expm1(randomNum)}`});
+    res.render('computation', { title: 'Jyothsna Pamarthi works with computation', 
+    result:`Math.expm1()  applied to ${randomNum} is ${Math.expm1(randomNum)}`,
+    result2:`Math.exp()  applied to ${randomNum} is ${Math.exp(randomNum)}`,
+    result3:`Math.atan()  applied to ${randomNum} is ${Math.atan(randomNum)}`,
+});
   //res.send(endRes);
 });
 
-
 module.exports = router;
+
