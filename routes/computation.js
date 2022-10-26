dc8cad3ba8bf3349a228eee43b57af9482fd81ff
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-10-26 14:02:56 -0500

    updated random num inside function

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var randomNum = Math.floor(Math.random() * 10);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('computation', { title: 'Jyothsna Pamarthi works with computation', result:`Math.expm1()  applied to ${randomNum} is ${Math.expm1(randomNum)}`});
+    var randomNum = Math.floor(Math.random() * 10);
+    res.render('computation', { title: 'Jyothsna Pamarthi works with computation', result:`Math.expm1()  applied to ${randomNum} is ${Math.expm1(randomNum)}`});
   //res.send(endRes);
 });
 
