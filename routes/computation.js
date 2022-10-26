cd9870382e1613946fe77cc95222b3f665ae8e37
jyothsna9797 <pamarthi.jyothsna97@gmail.com>
2022-10-26 13:29:17 -0500

    committed computational changes to git

--- routes/computation.js
+++ routes/computation.js
@@ -4,9 +4,8 @@ var randomNum = Math.random();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('computation', { title: 'Jyothsna Pamarthi works with computation' });
-  var endRes = `${Math.expm1()}  applied to ${randomNum} is ${Math.expm1(randomNum)}`
-  res.send(endRes);
+  res.render('computation', { title: 'Jyothsna Pamarthi works with computation', result:`${Math.expm1()}  applied to ${randomNum} is ${Math.expm1(randomNum)}`});
+  //res.send(endRes);
 });
 
 
